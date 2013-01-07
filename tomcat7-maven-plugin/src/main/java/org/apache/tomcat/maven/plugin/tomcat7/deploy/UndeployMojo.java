8b077315c2db009ddd59cf0a8a5376651f51cdfc
Olivier Lamy <olamy@apache.org>
2013-01-07 21:41:24 +0000

    [MTOMCAT-163] No tomcat7 undeploy goal Submitted by Tony Chemit.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/maven-plugin/trunk@1430030 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tomcat7-maven-plugin/src/main/java/org/apache/tomcat/maven/plugin/tomcat7/deploy/UndeployMojo.java
@@ -0,0 +1,78 @@
+package org.apache.tomcat.maven.plugin.tomcat7.deploy;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.tomcat.maven.common.deployer.TomcatManagerException;
+import org.apache.tomcat.maven.plugin.tomcat7.AbstractCatalinaMojo;
+
+import java.io.IOException;
+
+/**
+ * Undeploy a WAR from Tomcat.
+ *
+ * @goal undeploy
+ * @since 2.1
+ */
+@Mojo( name = "undeploy" )
+public class UndeployMojo
+    extends AbstractCatalinaMojo
+{
+    // ----------------------------------------------------------------------
+    // Mojo Parameters
+    // ----------------------------------------------------------------------
+
+    /**
+     * Whether to fail the build if the web application cannot be undeployed.
+     */
+    @Parameter( property = "maven.tomcat.failOnError", defaultValue = "true" )
+    private boolean failOnError;
+
+    // ----------------------------------------------------------------------
+    // Protected Methods
+    // ----------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void invokeManager()
+        throws MojoExecutionException, TomcatManagerException, IOException
+    {
+        getLog().info( messagesProvider.getMessage( "UndeployMojo.undeployingApp", getDeployedURL() ) );
+
+        try
+        {
+            log( getManager().undeploy( getPath() ).getHttpResponseBody() );
+        }
+        catch ( TomcatManagerException e )
+        {
+            if ( failOnError )
+            {
+                throw e;
+            }
+
+            getLog().warn( messagesProvider.getMessage( "UndeployMojo.undeployError", e.getMessage() ) );
+        }
+    }
+
+}
