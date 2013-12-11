4bde8b484363461660f5363aa21f09f4128b6598
Joshua Chaitin-Pollak <josh@offthehill.org>
2013-12-10 23:14:30 -0600

    Added a new parameter, “sessionManagerFactory”. If you specify a class that implements a createSessionManager() function, this class will be called and used to construct the session manager you want. This is useful for having your standalone instance use the memcached-session-manager project.

--- tomcat7-war-runner/src/main/java/org/apache/tomcat/maven/runner/Tomcat7RunnerCli.java
+++ tomcat7-war-runner/src/main/java/org/apache/tomcat/maven/runner/Tomcat7RunnerCli.java
@@ -83,6 +83,10 @@ public class Tomcat7RunnerCli
 
     static Option extractDirectory = OptionBuilder.withArgName( "extractDirectory" ).hasArg().withDescription(
         "path to extract war content, default value: .extract" ).create( "extractDirectory" );
+    
+    static Option sessionManagerFactoryClassName = OptionBuilder.withArgName( "className" ).hasArg().withDescription(
+        "classname of a factory that creates a session manager" ).create( "sessionManagerFactory" );
+
 
     static Option loggerName = OptionBuilder.withArgName( "loggerName" ).hasArg().withDescription(
         "logger to use: slf4j to use slf4j bridge on top of jul" ).create( "loggerName" );
@@ -97,7 +101,7 @@ public class Tomcat7RunnerCli
         options.addOption( httpPort ).addOption( httpsPort ).addOption( ajpPort ).addOption( serverXmlPath ).addOption(
             resetExtract ).addOption( help ).addOption( debug ).addOption( sysProps ).addOption(
             httpProtocol ).addOption( clientAuth ).addOption( keyAlias ).addOption( obfuscate ).addOption(
-            extractDirectory ).addOption( loggerName ).addOption( uriEncoding );
+            extractDirectory ).addOption(sessionManagerFactoryClassName).addOption( loggerName ).addOption( uriEncoding );
     }
 
 
@@ -196,6 +200,11 @@ public class Tomcat7RunnerCli
         if ( line.hasOption( extractDirectory.getOpt() ) )
         {
             tomcat7Runner.extractDirectory = line.getOptionValue( extractDirectory.getOpt() );
+        }
+        
+        if ( line.hasOption( sessionManagerFactoryClassName.getOpt() ) )
+        {
+            tomcat7Runner.sessionManagerFactoryClassName = line.getOptionValue( sessionManagerFactoryClassName.getOpt() );
         }
 
         if ( line.hasOption( loggerName.getOpt() ) )
