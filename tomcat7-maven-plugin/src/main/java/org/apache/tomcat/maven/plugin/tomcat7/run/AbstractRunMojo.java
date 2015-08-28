1fca8b009056c92a21a07432e6d014393e26477b
Benoit Meriaux <bmeriaux@octo.com>
2015-08-28 19:24:06 +0200

    use provided encoding for https conector

--- tomcat7-maven-plugin/src/main/java/org/apache/tomcat/maven/plugin/tomcat7/run/AbstractRunMojo.java
+++ tomcat7-maven-plugin/src/main/java/org/apache/tomcat/maven/plugin/tomcat7/run/AbstractRunMojo.java
@@ -1202,6 +1202,8 @@ public abstract class AbstractRunMojo
 
                     httpsConnector.setAttribute( "clientAuth", clientAuth );
 
+                    httpsConnector.setURIEncoding( uriEncoding );
+
                     httpsConnector.setUseBodyEncodingForURI( this.useBodyEncodingForURI );
 
                     if ( address != null )
