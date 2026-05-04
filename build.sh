87f9a4be875fd43e973768b07538db5f84c4a415
remm <remm@apache.org>
2026-05-04 08:22:52 +0200

    Update version number

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-export MAVEN_OPTS="-javaagent:$HOME/.m2/repository/org/jacoco/org.jacoco.agent/0.8.13/org.jacoco.agent-0.8.13-runtime.jar=destfile=/tmp/jacoco/jacoco-it.exec --add-opens=java.base/java.util.zip=ALL-UNNAMED --add-opens=java.base/java.util.jar=ALL-UNNAMED --add-opens=java.base/sun.net.www.protocol.http=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED"
+export MAVEN_OPTS="-javaagent:$HOME/.m2/repository/org/jacoco/org.jacoco.agent/0.8.14/org.jacoco.agent-0.8.14-runtime.jar=destfile=/tmp/jacoco/jacoco-it.exec --add-opens=java.base/java.util.zip=ALL-UNNAMED --add-opens=java.base/java.util.jar=ALL-UNNAMED --add-opens=java.base/sun.net.www.protocol.http=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED"
 
 # Run the main build lifecycle + integration tests
 mvn clean install failsafe:integration-test invoker:integration-test
